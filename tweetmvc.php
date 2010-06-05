1446437dd5ea131b60170216dd6c76cf13084a7d
Jeremy Lindblom <jeremeamia@gmail.com>
2010-06-05 13:21:45 -0700

    Fixed a parse error in the cookie extension.

--- tweetmvc.php
+++ tweetmvc.php
@@ -1,4 +1,4 @@
-<?const VERSION='v0.8.0';const EXT='.php';#TweetMVC Framework by Jeremy Lindblom - Framework core, setup, and request handler.
+<?const VERSION='v0.8.1';const EXT='.php';#TweetMVC Framework by Jeremy Lindblom - Framework core, setup, and request handler.
 error_reporting(341);function c($k){static$c;$c=$c?:include config.EXT;return$c[$k];}foreach(c(extensions)as$f)require c(x).$f.EXT;         #Setup
 class M{static function factory($c,$i=0){require_once c(m).$c.EXT;$m=new$c;return$m->load($c,$i);}function load($c,$i){return$this;}}       #Model
 class V{function V($f){$this->f=$f;}function __toString(){ob_start();extract((array)$this);require c(v).$f.EXT;return ob_get_clean();}}     #View
