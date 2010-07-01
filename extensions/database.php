e32280085598485a01c8550f9eb4fe4c18860603
Jeremy Lindblom <jeremeamia@gmail.com>
2010-07-01 10:08:11 -0700

    replacing addslashes() with mysql_real_escape_string(). This is more secure, and it also fits now. Past iterations did not allow for this lengthy function.

--- extensions/database.php
+++ extensions/database.php
@@ -7,7 +7,7 @@ function d($c,$i=0){return q(4,$c,W.(\is_int($i)?(!$i?'1':"id=$i"):$i));}functio
 function r($c,$i){return($o=q(1,$c,W."id=$i LIMIT 1")->fetch_object($c))?$o:e($c);}function l($l){return' LIMIT '.($l[1]?"$l[0],$l[1]":$l);}#Single Row, Limit
 function e($c){$o=new$c;$r=q(0,$c);while($f=$r->fetch_row())$o->$f[0]=NULL;return$o;}function w($w){return m($w)?W.m(a($w,'`%s`='),A):$w;}  #Get Empty Row
 function t($s,$t){$a=array("SHOW COLUMNS FROM $t","SELECT $t.* FROM $t","INSERT INTO $t","UPDATE $t SET","DELETE FROM $t");return$a[$s];}   #SQL query type
-function v(&$v,$k=0,$p=0){return$v=($p?\sprintf($p,$k):'').(\is_numeric($v)?$v:($v===NULL?'NULL':'"'.\addslashes($v).'"'));}                #Prepare a value
+function v(&$v,$k=0,$p=0){return$v=($p?\sprintf($p,$k):'').(\is_numeric($v)?$v:($v===NULL?'NULL':'"'.\mysql_real_escape_string($v).'"'));}  #Prepare a value
 function j($t0,$t1,$f=1){return" INNER JOIN $t1 ON ".($f?"$t0.id=$t1.".$t0._id:"$t0.{$t1}_id=$t1.id");}                                     #Format a join
 function g($t0,$t1=0){$g=@array($t0=\get_class($t0)?:$t0,$t1=\get_class($t1)?:$t1);\sort($g);return array(m($g,_to_),$t0,$t1);}             #Get glue table
 function q($s,$c=0,$w=''){$q=!$c?$s:t($s,$c).$w;return c()->query($q);}function f(&$a){$a=(array)$a;unset($a[relationships]);}              #Perform query, filter fields
