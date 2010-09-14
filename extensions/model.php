5c4ce91b4fdde77b317ccabfb0778ebc3fdec37a
Jeremy <jeremy@synapsestudios.com>
2010-09-13 18:26:39 -0700

    Model extensions now assumes the existence of a $this->_table property which contains the DB table name. Also, the APIs for the load() and load_all() method have been changed

--- extensions/model.php
+++ extensions/model.php
@@ -1,7 +1,7 @@
 <?#Model Extensions - Incrementally increase the capabilities of the Model into an ORM
-class ML extends M{function load($c,$i=0){return db\r($c,$i);}function load_all($w='',$b=0,$l=0){return db\s(get_class($this),$w,$b,$l);}}  #Loadable Model
-class MLR extends ML{function related($k){M::factory($k);$m='db\\'.$this->relationships[$k];return$m(get_class($this),$k,$this);}}          #Loadable Relationships Model
-class MS extends MLR{function save(){$c=get_class($t=$this);$r=($i=&$t->id)?db\u($c,$t,$i):db\i($c,$t);$i=$i?:db\c()->insert_id;return$r;}} #Savable Model
+class ML extends M{function load($i){return db\r($this->_table,$i);}function load_all($w=0,$b=0,$l=0){return db\s($this->_table,$w,$b,$l);}}#Loadable Model
+class MLR extends ML{function related($k){M::factory($k);$m='db\\'.$this->_relationships[$k];return$m($this->_table,$k,$this);}}            #Loadable Relationships Model
+class MS extends MLR{function save(){$t=$this;$c=$t->table;$r=($i=&$t->id)?db\u($c,$t,$i):db\i($c,$t);$i=$i?:db\c()->insert_id;return$r;}}  #Savable Model
 class MSR extends MS{function add(M$o){list($g,$c,$f)=db\g($t=$this,$o);return@db\i($g,array($c._id=>$t->id,$f._id=>$o->id));}}             #Savable Relationships Model
-class MD extends MSR{function delete(){return db\d(get_class($this),$this->id);}function delete_all(){return db\d(get_class($this));}}      #Deletable Model
+class MD extends MSR{function delete(){return db\d($this->_table,$this->id);}function delete_all(){return db\d($this->_table);}}            #Deletable Model
 class MDR extends MD{function remove(M$o){list($g,$c,$f)=db\g($this,$o);return@db\d($g,$c."_id=$this->id AND {$f}_id=$o->id");}}            #Deletable Relationships Model
